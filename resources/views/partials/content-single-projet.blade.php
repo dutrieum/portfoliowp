59bb6eabb2feb8830cc3eeeab4982b60e5d47ffe
Mathilde <dutrieuxmathilde@gmail.com>
2021-04-10 21:02:31 +0200

    Design projet page

--- resources/views/partials/content-single-projet.blade.php
+++ resources/views/partials/content-single-projet.blade.php
@@ -1,48 +1,54 @@
 <main>
     <h1>{{the_field('project_title')}}</h1>
     <div class="context">
-        <div>
-            <h2>Qui ?</h2>
-            <p>{{the_field('who_content')}}</p>
+        <div class="cont_first_half">
+            <div>
+                <h2>Qui ?</h2>
+                <p>{{the_field('who_content')}}</p>
+            </div>
+            <div>
+                <h2>Quoi ?</h2>
+                <p>{{the_field('what_content')}}</p>
+            </div>
         </div>
-        <div>
-            <h2>Quoi ?</h2>
-            <p>{{the_field('what_content')}}</p>
+        <div class="cont_second_half">
+            <div>
+                <h2>Quand ?</h2>
+                <p>{{the_field('when_content')}}</p>
+            </div>
+            <div>
+                <h2>Comment ?</h2>
+                <p>{{the_field('how_content')}}</p>
+            </div>
         </div>
-        <div>
-            <h2>Quand ?</h2>
-            <p>{{the_field('when_content')}}</p>
-        </div>
-        <div>
-            <h2>Comment ?</h2>
-            <p>{{the_field('how_content')}}</p>
-        </div>
-        <h3>Catégories :
-            @foreach (get_the_terms($post->id,"categorie") as $term)
-                {{$term->name}}
-                @if(!$loop->last)
-                ,
-                @endif
-            @endforeach
-        </h3>
+    </div>
+    <div class="category">
+        <h2>Catégorie(s) : </h2>
+        <p>@foreach (get_the_terms($post->id,"categorie") as $term)
+            {{$term->name}}
+            @if(!$loop->last)
+            ,
+            @endif
+        @endforeach</p>
     </div>
     <div class="image-text">
         <div class="image">
-            {!! wp_get_attachment_image( get_field("first_image")["id"]) !!}
+            {!! wp_get_attachment_image( get_field("first_image")["id"], "medium" ) !!}
         </div>
         <div class="text">
             <p>{{the_field('first_description')}}</p>
         </div>
-    </div><div class="image-text">
+    </div>
+    <div class="image-text">
         <div class="text">
             <p>{{the_field('second_description')}}</p>
         </div>
         <div class="image">
-            {!! wp_get_attachment_image( get_field("second_image")["id"]) !!}
+            {!! wp_get_attachment_image( get_field("second_image")["id"], "medium" ) !!}
         </div>
     </div>
     @php $link_media = get_field('external_link') @endphp
     @if ($link_media != null)
-        <a href="{{ $link_media['url'] }}" target="{{ $link_media['target'] }}">{{ $link_media['title'] }}</a>
+        <a href="{{ $link_media['url'] }}" target="{{ $link_media['target'] }}" class="projet_link">{{ $link_media['title'] }}</a>
     @endif
 </main>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,21 @@
 <footer class="content-info">
   <div class="container">
     <!-- @php dynamic_sidebar('sidebar-footer') @endphp -->
+    <h2>Contact</h2>
+    <div class="footer_info">
+      <div class="info_single">
+        <h3>ADRESSE</h3>
+        <p>Rue Croix d'Or Chambéry</p>
+      </div>
+      <div class="info_single">
+        <h3>MAIL</h3>
+        <p>dutrieuxmathilde@gmail.com</p>
+      </div>
+      <div class="info_single">
+        <h3>TÉLÉPHONE</h3>
+        <p>+00 00 00 00 00</p>
+      </div>
+    </div>
+    <p class="credits">Copyright © Mathilde Dutrieux - 2021</p>
   </div>
 </footer>
