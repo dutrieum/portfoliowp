12562120b68245124814d16773cc58238148c65a
Mathilde <dutrieuxmathilde@gmail.com>
2021-04-12 19:35:31 +0200

    Add 404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -3,10 +3,16 @@
 @section('content')
   @include('partials.page-header')
 
-  @if (!have_posts())
+  <!-- @if (!have_posts())
     <div class="alert alert-warning">
       {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
     </div>
     {!! get_search_form(false) !!}
-  @endif
+  @endif -->
+
+  <div class="error_page">
+    <p>La page que vous cherchez n'existe pas !</p>
+    <a href="{{ home_url('/') }}">Cliquez ici pour revenir à l'accueil.</a>
+  </div>
+
 @endsection
